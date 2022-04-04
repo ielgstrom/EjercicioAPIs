98ac292bcee1b7643a80682194966036115b1666
Ignasi Elgström Puyuelo <83967813+ielgstrom@users.noreply.github.com>
2022-04-04 13:41:56 +0200

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChuckNorris;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var response = API.Random(exclude: new string[] { "explicit" });
+            var joke = response.Result;
+            richTextBox1.Text = joke.Text;
+        }
+    }
+}
